3655ee93bd78b0d307cdf7a6092bb5a8fa1ca157
thomrick <tricart@xebia.fr>
2017-05-25 15:43:02 +0200

    need to manage project lib structure to integrate lib components

--- examples/servers/ts/src/index.ts
+++ examples/servers/ts/src/index.ts
@@ -1,27 +1,30 @@
 import * as express from 'express';
-import {Application, Router} from 'express';
 import {UserController} from './user.controller';
 
 class ApplicationStarter {
   public static main(): void {
-    const application: Application = express();
+    const application: express.Application = express();
     application.use(ApplicationStarter.getRouter());
     //application.use(ApplicationStarter.getApiDiscoveryRouter());
     this.start(application);
   }
 
-  private static getRouter(): Router {
-    const router: Router = express.Router();
+  private static getRouter(): express.Router {
+    const router: express.Router = express.Router();
     const controller: UserController = new UserController();
     router.get('/api/users', controller.getAll.bind(controller));
     return router;
   }
 
-  private static getApiDiscoveryRouter(): Router {
-    return null;
+  /*
+  private static getApiDiscoveryRouter(): express.Router {
+    return new Router('/api/map').create([
+      UserController
+    ]);
   }
+  */
 
-  private static start(application: Application): void {
+  private static start(application: express.Application): void {
     application.listen(8080, () => {
       console.log('Application server listening at port 8080');
     });
