b0b07e87fc84884e2dfa443ccf30f004a460cb01
thomrick <tricart@xebia.fr>
2017-05-25 13:55:09 +0200

    #FEATURE-0008 develop the UriTemplateMapper to be integrated to the Scanner

--- src/server/core/uri-template/uri-template.mapper.spec.ts
+++ src/server/core/uri-template/uri-template.mapper.spec.ts
@@ -2,17 +2,33 @@ import {UriTemplateMapper} from './uri-template.mapper';
 import {expect} from 'chai';
 
 describe('UriTemplateMapper', () => {
+  let mapper: UriTemplateMapper;
+
+  beforeEach(() => {
+    mapper = new UriTemplateMapper();
+  });
+
   it('can be created', () => {
-    const mapper = new UriTemplateMapper();
     expect(mapper).to.exist;
   });
 
-  it('can call build()', () => {
-    const mapper = new UriTemplateMapper();
-    expect(mapper.mapper).to.exist;
-  });
-
   describe('#map()', () => {
+    it('can call map()', () => {
+      mapper.map('uri');
+    });
 
+    it('should return an UriTemplate not templated', () => {
+      expect(mapper.map('/api/path')).to.be.deep.equal({
+        url: '/api/path',
+        templated: false
+      });
+    });
+
+    it('should return an UriTemplate templated', () => {
+      expect(mapper.map('/api/path/:path-param')).to.be.deep.equal({
+        url: '/api/path/:path-param',
+        templated: true
+      });
+    });
   });
 });
--- src/server/core/uri-template/uri-template.mapper.ts
+++ src/server/core/uri-template/uri-template.mapper.ts
@@ -1,3 +1,16 @@
+import {UriTemplate} from '../../../common/interfaces/uri-template';
+
 export class UriTemplateMapper {
-  public mapper() {};
+  private TEMPLATE_REGEX = /:/g;
+
+  public map(uri: string): UriTemplate {
+    return {
+      url: uri,
+      templated: this.isTemplated(uri)
+    };
+  }
+
+  private isTemplated(uri: string): boolean {
+    return this.TEMPLATE_REGEX.test(uri);
+  }
 }
